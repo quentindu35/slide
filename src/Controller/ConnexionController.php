a5727a92758008fa9131e5e304f1f64d48beb5d6
Quentin Grosdoigt <grosdoigtquentin35@gmail.com>
2020-04-22 11:38:32 +0200

    Premier commit

---
+++ src/Controller/ConnexionController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConnexionController extends AbstractController
+{
+    /**
+     * @Route("/connexion", name="connexion")
+     */
+    public function index()
+    {
+        return $this->render('connexion/index.html.twig', [
+            'controller_name' => 'ConnexionController',
+        ]);
+    }
+
+    /**
+     * @Route("/connexion/log", name="connexion-log")
+     */
+    public function log()
+    {
+        $data = [
+            'email' => $_POST['user_mail'],
+            'mdp' => $_POST['mdp'],
+        ];
+        var_dump($data);die();
+        return $this->render('connexion/index.html.twig', [
+            'controller_name' => 'ConnexionController',
+        ]);
+    }
+}
---
+++ src/Controller/InscriptionController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class InscriptionController extends AbstractController
+{
+    /**
+     * @Route("/inscription", name="inscription")
+     */
+    public function index()
+    {
+        return $this->render('inscription/index.html.twig', [
+            'controller_name' => 'InscriptionController',
+        ]);
+    }
+
+    /**
+     * @Route("/inscription/insert", name="inscription")
+     */
+    public function insert()
+    {
+        $data = [
+            'nom' => $_POST['nom'],
+            'prenom' => $_POST['prenom'],
+            'email' => $_POST['user_mail'],
+            'mdp' => $_POST['mdp'],
+        ];
+        var_dump($data);die();
+        return $this->render('inscription/index.html.twig', [
+            'controller_name' => 'InscriptionController',
+        ]);
+    }
+}
