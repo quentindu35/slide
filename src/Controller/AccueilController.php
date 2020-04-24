6acf1254c3c56c3f969e82cc127d7d7832c3f1e6
Quentin Grosdoigt <grosdoigtquentin35@gmail.com>
2020-04-24 15:15:18 +0200

    Premier commit

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Photo;
+use App\Form\PhotoType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Slide;
 
@@ -15,12 +18,36 @@ class AccueilController extends AbstractController
     /**
      * @Route("/accueil", name="accueil")
      */
-    public function index(EntityManagerInterface $em)
+    public function index(EntityManagerInterface $em, Request $request)
     {
+        $entityManager = $this->getDoctrine()->getManager();
+        $upload = new Photo();
+        $form = $this->createForm(PhotoType::class, $upload);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $file = $upload->getNom();
+            $extention = $file->guessExtension();
+            $fileName = md5(uniqid());
+            $file->move($this->getParameter('upload_directory'), $fileName);
+            $upload->setNom($fileName);
+            $upload->setExtention(".".$extention);
+            $upload->setDatedAjout(new \DateTime());
+            $entityManager->persist($upload);
+            $entityManager->flush();
+
+
+
+            return $this->redirectToRoute('accueil');
+        }
         $slides = $em->getRepository(Slide::class)->findAll();
+        $image =$this->getDoctrine()
+            ->getRepository(Photo::class)
+            ->findAll();
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
             'slides' => $slides,
+            'form' => $form->createView(),
+            'images' => $image,
         ]);
     }
 }
